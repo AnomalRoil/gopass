1792a01f93a1f9647ca4902bd72673f42d7049f7
Yolan Romailler <anomalroil@users.noreply.github.com>
2023-05-31 19:51:27 +0200

    Gofumpt-ing
    Signed-off-by: Yolan Romailler <anomalroil@users.noreply.github.com>

--- pkg/otp/screenshot.go
+++ pkg/otp/screenshot.go
@@ -4,11 +4,12 @@ package otp
 
 import (
 	"context"
+	"strings"
+
 	"github.com/gopasspw/gopass/internal/out"
 	"github.com/kbinani/screenshot"
 	"github.com/makiuchi-d/gozxing"
 	"github.com/makiuchi-d/gozxing/qrcode"
-	"strings"
 )
 
 // ParseScreen will attempt to parse all available screen and will look for otpauth QR codes. It returns the first one

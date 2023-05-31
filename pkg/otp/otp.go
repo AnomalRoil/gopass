d42e8cd6707044fef1a1d7a5e4d6fe6ca61e8d55
Yolan Romailler <anomalroil@users.noreply.github.com>
2023-05-31 15:34:53 +0200

    [CLEANUP] Fixing issues from linter
    Signed-off-by: Yolan Romailler <anomalroil@users.noreply.github.com>

--- pkg/otp/otp.go
+++ pkg/otp/otp.go
@@ -2,10 +2,15 @@ package otp
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"image/png"
 	"os"
 	"strings"
+
+	"github.com/kbinani/screenshot"
+	"github.com/makiuchi-d/gozxing"
+	"github.com/makiuchi-d/gozxing/qrcode"
 
 	"github.com/gopasspw/gopass/internal/out"
 	"github.com/gopasspw/gopass/pkg/debug"
@@ -85,6 +90,44 @@ func parseOTP(typ string, secKey string) (*otp.Key, error) {
 	}
 
 	return key, nil
+}
+
+// ParseScreen will attempt to parse all available screen and will look for otpauth QR codes. It returns the first one
+// it has found.
+func ParseScreen(ctx context.Context) (string, error) {
+	var qr string
+	for i := 0; i < screenshot.NumActiveDisplays(); i++ {
+		out.Noticef(ctx, "Scanning screen n°%d", i)
+
+		img, err := screenshot.CaptureDisplay(i)
+		if err != nil {
+			return "", err
+		}
+
+		out.OKf(ctx, "Area scanned on screen n°%d: %v", i, img.Bounds())
+		bmp, err := gozxing.NewBinaryBitmapFromImage(img)
+		if err != nil {
+			return "", err
+		}
+		qrReader := qrcode.NewQRCodeReader()
+		result, err := qrReader.Decode(bmp, nil)
+		if err != nil {
+			out.Warningf(ctx, "No QR code found while parsing screen n°%d.", i)
+
+			continue
+		}
+
+		out.Noticef(ctx, "Found a qrcode, checking.")
+		if strings.HasPrefix(result.GetText(), "otpauth://") {
+			qr = result.GetText()
+			out.OKf(ctx, "Found an otpauth:// QR code on screen n°%d (%v)", i, img.Bounds())
+
+			break
+		}
+		out.Warningf(ctx, "Not an otpauth:// QR code, please make sure to only have your OTP qrcode displayed.")
+	}
+
+	return qr, nil
 }
 
 // WriteQRFile writes the given OTP code as a QR image to disk.
